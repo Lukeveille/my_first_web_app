464bb5c6cc338fe3cda3812afd92deacdac4bad7
Lukeveille <luke@lukeleveille.com>
2018-09-20 17:58:31 -0400

    Completed main exercise

--- server.rb
+++ server.rb
@@ -1,7 +1,15 @@
 require "sinatra"
 
+get "/" do
+  redirect to("/home")
+end
+
 get "/home" do
   erb :index
+end
+
+get "/gallery" do
+  redirect to("/portfolio")
 end
 
 get "/portfolio" do
@@ -9,5 +17,12 @@ get "/portfolio" do
 end
 
 get "/about_me" do
+  @skills = ["git", "HTML", "CSS", "Ruby"]
+  @interests = ["cats", "art", "music", "films", "coffee"]
   erb :about_me
+end
+
+get "/favourites" do
+  @fav_links = ["http://www.reddit.com/", "http://www.reuters.com", "http://www.edx.org", "http://sheets.google.com/", "http://www.gomovies.cool/movie/filter"]
+  erb :fav_links
 end
